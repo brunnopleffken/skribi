0a985172e828a8a76563f48502c50f93a708b8f5
Brunno Pleffken <brunno.pleffken@outlook.com>
2020-06-20 03:12:30 -0300

    Add some models to get started

---
+++ Skribi/Models/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Skribi.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public virtual DbSet<Room> Rooms { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Skribi/Models/Room.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Skribi.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
