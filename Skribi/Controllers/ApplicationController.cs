3321f74cc49d68bd12df5d43f3d7a0aac16e29ec
Brunno Pleffken <brunno.pleffken@outlook.com>
2020-06-20 03:20:12 -0300

    Refactor some properties to ApplicationController

---
+++ Skribi/Controllers/ApplicationController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using Skribi.Models;
+
+namespace Skribi.Controllers
+{
+    public class ApplicationController : Controller
+    {
+        protected ApplicationDbContext _context;
+    }
+}
--- Skribi/Controllers/HomeController.cs
+++ Skribi/Controllers/HomeController.cs
@@ -1,20 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
-using Skribi.ViewModels;
 using Skribi.Models;
 
 namespace Skribi.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : ApplicationController
     {
-        private readonly ApplicationDbContext _context;
-
         public HomeController(ApplicationDbContext context)
         {
             _context = context;
