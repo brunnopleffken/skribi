24d58ef0d72c2714857affa95692cf6852859e02
Brunno Pleffken <brunno.pleffken@outlook.com>
2020-06-20 19:00:23 -0300

    Add missing relation Thread.RoomId

--- Skribi/Models/Thread.cs
+++ Skribi/Models/Thread.cs
@@ -6,6 +6,9 @@ namespace Skribi.Models
     {
         public int Id { get; set; }
 
+        public int RoomId { get; set; }
+        public Room Room { get; set; }
+
         public int UserId { get; set; }
         public User User { get; set; }
 
