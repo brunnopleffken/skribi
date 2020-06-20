03ece8dd0451d3321673766308c7db989e5028b3
Brunno Pleffken <brunno.pleffken@outlook.com>
2020-06-20 03:12:59 -0300

    View Home and list of rooms

---
+++ Skribi/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Skribi.ViewModels;
+using Skribi.Models;
+
+namespace Skribi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var rooms = await _context.Rooms.ToListAsync();
+            return View(rooms);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
