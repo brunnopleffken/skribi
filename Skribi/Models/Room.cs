a00c727f7f27f640cdaf09654e65c6202623334d
Brunno Pleffken <brunno.pleffken@outlook.com>
2020-06-22 02:28:42 -0300

    Add to model Room.Position

--- Skribi/Models/Room.cs
+++ Skribi/Models/Room.cs
@@ -8,6 +8,7 @@ namespace Skribi.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public int Position { get; set; }
 
         public List<Thread> Threads { get; set; }
     }
