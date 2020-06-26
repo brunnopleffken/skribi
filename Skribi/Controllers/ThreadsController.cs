b3e2d1becd6ab0c37a7d99a05ef17664e1b3b7c2
Brunno Pleffken <brunno.pleffken@outlook.com>
2020-06-25 21:21:14 -0300

    Add view thread page

---
+++ Skribi/Controllers/ThreadsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Skribi.Models;
+using Skribi.ViewModels;
+
+namespace Skribi.Controllers
+{
+    public class ThreadsController : ApplicationController
+    {
+        public ThreadsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(int id)
+        {
+            ThreadViewModel model = new ThreadViewModel
+            {
+                Thread = await _context.Threads
+                    .Include(t => t.User)
+                    .Include(t => t.Room)
+                    .SingleAsync(t => t.Id == id),
+
+                Replies = await _context.Replies
+                    .Where(r => r.ThreadId == id)
+                    .Include(r => r.User)
+                    .ToListAsync()
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ Skribi/ViewModels/ThreadViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Skribi.Models;
+
+namespace Skribi.ViewModels
+{
+    public class ThreadViewModel
+    {
+        public Thread Thread { get; set; }
+        public List<Reply> Replies { get; set; }
+    }
+}
